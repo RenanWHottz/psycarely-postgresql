21cee462a94e6843130964295be589a0c36ba041
renanwhottz <renanhottz2003@gmail.com>
2025-11-17 19:44:11 -0300

    Ultimo commit antes da mudança para postgresql.

--- core/settings.py
+++ core/settings.py
@@ -100,17 +100,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'psycarely',
-        'USER': 'root',
-        'PASSWORD': '',  
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
